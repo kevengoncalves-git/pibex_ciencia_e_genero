f53dd7a2e32d66da41ea9fa4566e8375c4568347
Keven <kevenkaue25@gmail.com>
2026-08-17 19:11:59 -0300

    Edição no home, cadastro e login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_cors import CORS
 
 from config import Config
@@ -20,22 +20,39 @@ def create_app():
     def home():
         return render_template("index.html")
 
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
+    @app.route("/home")
+    def home_page():
+        return render_template("home.html")
 
-# Rota que recebe os dados do formulário (POST)
-@app.route('/inscrever', methods=['POST'])
-def inscrever():
-    nome = request.form.get('nome')
-    vinculo = request.form.get('vinculo')
-    percepcao = request.form.get('percepcao')
+    @app.route("/materiais")
+    def materiais():
+        return render_template("materiais.html")
 
-    # Exemplo de salvamento no PostgreSQL
-    # conn = psycopg2.connect("dbname=pibex user=postgres password=...")
-    # cur = conn.cursor()
-    # cur.execute("INSERT INTO inscritos (nome, vinculo, percepcao) VALUES (%s, %s, %s)", (nome, vinculo, percepcao))
-    # conn.commit()
+    @app.route("/cadastro")
+    def cadastro_page():
+        return render_template("cadastro.html")
+
+    @app.route("/login")
+    def login_page():
+        return render_template("login.html")
+
+    # Rota que recebe os dados do formulário de percepção (POST)
+    @app.route("/inscrever", methods=["POST"])
+    def inscrever():
+        nome = request.form.get("nome")
+        vinculo = request.form.get("vinculo")
+        percepcao = request.form.get("percepcao")
+
+        # Exemplo de salvamento no PostgreSQL
+        # conn = psycopg2.connect("dbname=pibex user=postgres password=...")
+        # cur = conn.cursor()
+        # cur.execute("INSERT INTO inscritos (nome, vinculo, percepcao) VALUES (%s, %s, %s)", (nome, vinculo, percepcao))
+        # conn.commit()
+
+        return f"Obrigado, {nome}! Sua percepção foi registrada para o projeto."
+
+    with app.app_context():
+        db.create_all()
 
     return app
 
